7d7888e4f5dcca2f1792df49113a9bd0cb1fff25
Alessio Michelini <darkmavis1980@gmail.com>
2025-04-26 15:17:28 +0100

    feat: updated logic for sendgrid driver

--- apps/server/src/integrations/mail/drivers/sendgrid.driver.ts
+++ apps/server/src/integrations/mail/drivers/sendgrid.driver.ts
@@ -1,14 +1,15 @@
-import Client from '@sendgrid/client';
-import sgMail from '@sendgrid/mail';
+import sgMail, { MailDataRequired, MailService } from '@sendgrid/mail';
 import { Logger } from '@nestjs/common';
 import { MailDriver } from './interfaces/mail-driver.interface';
 import { SendGridConfig } from '../interfaces';
 import { MailMessage } from '../interfaces/mail.message';
 import { mailLogName } from '../mail.utils';
 
+console.info(sgMail);
+
 export class SendGridDriver implements MailDriver {
   private readonly logger = new Logger(mailLogName(SendGridDriver.name));
-  private readonly sendgridClient: Client;
+  private readonly sendgridClient: MailService;
 
   constructor(config: SendGridConfig) {
     this.sendgridClient = sgMail;
@@ -17,9 +18,8 @@ export class SendGridDriver implements MailDriver {
 
   async sendMail(message: MailMessage): Promise<void> {
     try {
-      await this.sendgridClient.send({
-        ...message,
-      });
+      const messageData: MailDataRequired = message as unknown as MailDataRequired;
+      await this.sendgridClient.send(messageData);
       this.logger.debug(`Sent mail to ${message.to}`);
     } catch (err) {
       this.logger.warn(`Failed to send mail to ${message.to}: ${err}`);
